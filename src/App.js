eb85bd0f188576e5431403d69410fd58faa709e3
Veronika <veroniksayas@gmail.com>
2022-11-28 14:57:28 +0200

    Created counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount((prevState) => prevState + 1);
+  };
+
+  const decrementCount = () => {
+    setCount((prevState) => prevState - 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <h2>Count</h2>
+        <h1>{count}</h1>
+        <button className="decrement" onClick={decrementCount}>
+          Decrement
+        </button>
+        <button className="increment" onClick={incrementCount}>
+          Increment
+        </button>
+        <button className="reset" onClick={resetCount}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
